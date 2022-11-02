22c277938d3bd71a4c4666ce1f689b10ac48f77b
Brent Picasso <brent@autosportlabs.com>
2022-11-01 21:10:19 -0700

    initial upload

---
+++ LVX/LVX/Vacuum Controller/Firmware/ver10/Watch_1.wat
@@ -0,0 +1,15 @@
+[Watch_Name]
+Watch_Name=Watch_1
+
+[Watch_Registers]
+0=BASELINE
+1=porta
+2=portb
+3=MODE.TEST
+4=READ_BASELINE.TEMP
+5=tmr0
+6=adres
+7=READ_BASELINE.X
+8=SAMPLING.SAMPLE
+9=TIME
+
---
+++ LVX/LVX/Vacuum Controller/Firmware/ver10/vs.c
@@ -0,0 +1,136 @@
+/*    Vaccuum Switch Controller    							*/
+/*    Revision 1.0 5/5/98          							*/
+
+#include "16C71.H"
+
+			//application specific definitions
+#define min_vac 20
+#define vac_solonoid PIN_B0		//input pc calling for vacuum
+#define vacuum_sense PIN_B1		//output component confirmation to pc
+#define vacuum_sol_drive PIN_B2		//output to turn vac sol on
+#define vac_generator PIN_B6		//output to turn vac gen on
+
+			//global definitions
+int baseline,time;
+
+
+
+#fuses XT,NOPROTECT,NOWDT
+#use DELAY(clock=4000000)
+#use Standard_IO(A)
+#use Standard_IO(B)
+
+
+void INITIALIZE()			//startup
+{
+	//set_tris_a(0x0F);		//=b'1111'
+	//set_tris_b(0xFF);		//=b'11111001'
+	setup_port_a(RA0_RA1_ANALOG);	//set up a2d
+	setup_adc(ADC_CLOCK_DIV_2);
+	set_adc_channel(0);
+	baseline = 100;
+	time = 32;			//each 16 is one second delay
+	output_high(vacuum_sense);
+	delay_ms(400);
+	output_low(vacuum_sense);
+	delay_ms(400);
+	output_high(vacuum_sense);
+	delay_ms(400);
+	output_low(vacuum_sense);
+	delay_ms(400);
+	output_high(vacuum_sense);
+	delay_ms(400);
+	output_low(vacuum_sense);
+}
+
+
+void SAMPLING()
+{	int sample;
+
+	output_high(vac_generator);
+	do
+	{	delay_us(400);				//400sets sample rate at 2500Hz
+		sample = read_adc()/2;			//takes average of two samples
+		delay_us(400);
+		sample = sample + read_adc()/2;
+		if (sample < baseline) output_low(vacuum_sense);
+		else output_high(vacuum_sense);
+	} while (!input(vac_solonoid));			//is solinoid on?
+	if (sample > baseline) 
+	{	
+ 		do
+		{delay_us(400);				//sets sample rate at 2500Hz
+		sample = read_adc()/2;			//takes average of two samples
+		delay_us(400);
+		sample = sample + read_adc()/2;
+		} while (sample > min_vac);		//is vacuum low?
+ 		output_low(vacuum_sense);
+	}
+	setup_counters(rtcc_internal,rtcc_div_256);	//sets up 2 second delay for generator
+	set_rtcc(1);
+	time = 0;
+	return;
+}
+
+
+void READ_BASELINE()
+{	int x;
+	long temp;
+
+	output_high(vac_generator);
+	output_high(vacuum_sol_drive);
+	delay_ms(300);
+	baseline = 0;
+	temp = 0;
+	for (x = 0;x != 8;x++){
+		delay_ms(2);
+		temp = temp + read_adc();
+		}
+	temp = temp / 8;				//take average of 8 samples
+	baseline = 4 + temp + (211 - temp)/8;		//baseline1 = 4 + baseline0 +.125*(maxvac-baseline0)
+	output_low(vacuum_sense);
+	output_low(vac_generator);
+	output_low(vacuum_sol_drive);
+	return;
+}
+
+
+void MODE()						//setup baseline or normal sample mode?
+{	int test,logic;
+
+	logic = 1;
+	test = 1;
+	setup_counters(rtcc_internal,rtcc_div_8);
+	set_rtcc(68);
+	do						//test to see 3 changes in state
+	{	if (input(vac_solonoid) == logic)
+		{	test = test + 1;
+			if (logic == 0) logic = 1;
+			else logic = 0;
+		}
+	} while (get_rtcc() > 67);			//check mode for 1.5ms
+	if (test == 5) output_high(vacuum_sense);
+	if (test >= 4) READ_BASELINE();			//if one or more toggle then goto setup
+	else SAMPLING();
+	return;
+}
+
+	
+TIMEOFF()						//check to see if 2 seconds has elapsed	
+{	if (time == 32) return;				//before the vacuum gen will be shut off
+	else
+	{	time++;
+		set_rtcc(1);
+		if (time == 32) output_low(vac_generator);
+		return;
+	}
+}
+
+MAIN()
+{	INITIALIZE();
+	do
+	{
+		if (!input(vac_solonoid)) MODE();
+		if (get_rtcc() == 0) TIMEOFF();
+	} while(true);	
+}
